69b4b1766bcdbfa113f54fda93e4bffa3efb92fe
Fritz Mehner <mehner.fritz@fh-swf.de>
2012-08-18 13:06:58 +0200

    archiving utility rewritten

--- perl-support/makedist.sh
+++ perl-support/makedist.sh
@@ -2,48 +2,68 @@
 #===============================================================================
 #
 #          FILE:  makedist.sh
-# 
-#         USAGE:  ./makedist.sh 
-# 
-#   DESCRIPTION:  
-# 
+#
+#         USAGE:  ./makedist.sh
+#
+#   DESCRIPTION:  create archive perl-support.zip
+#
 #       OPTIONS:  ---
 #  REQUIREMENTS:  ---
 #          BUGS:  ---
 #         NOTES:  ---
 #        AUTHOR:  Dr.-Ing. Fritz Mehner (Mn), mehner@fh-swf.de
 #       COMPANY:  Fachhochschule Südwestfalen, Iserlohn
-#       VERSION:  1.0
-#       CREATED:  12.05.2007 08:41:41 CEST
+#       VERSION:  2.0
+#       CREATED:  17.08.2012 15:01:05 CEST
 #===============================================================================
 
 archive_name="perl-support"
-exclude_list="makedist.sh hotkeys.latex/\* *.swp *.cvsignore"
-
-rm --force $archive_name".zip"
 
 #-------------------------------------------------------------------------------
 #   Hotkeys: PDF und LaTeX-Quelle kopieren
 #-------------------------------------------------------------------------------
-cp hotkeys.latex/perl-hot-keys.tex perl-support/doc/
-cp hotkeys.latex/perl-hot-keys.pdf perl-support/doc/
+cp hotkeys.latex/perl-hot-keys.tex doc/
+cp hotkeys.latex/perl-hot-keys.pdf doc/
 
 #-------------------------------------------------------------------------------
 #   persönliche Angaben aus dem Haupt-Template-File ändern
 #-------------------------------------------------------------------------------
-templatefile=./perl-support/templates/Templates
+MainTemplateFile=./templates/Templates
 
-if [ -f $templatefile ] ; then
-	sed --in-place  '/SetMacro.*AUTHOR/s/Dr. Fritz Mehner/YOUR NAME/'        $templatefile
-	sed --in-place  '/SetMacro.*AUTHORREF/s/fgm//'                           $templatefile
-	sed --in-place  '/SetMacro.*EMAIL/s/mehner.fritz@fh-swf.de//'            $templatefile
-	sed --in-place  '/SetMacro.*ORGANIZATION/s/FH Südwestfalen, Iserlohn//'  $templatefile
+if [ -f $MainTemplateFile ] ; then
+  sed --in-place  '/SetMacro.*AUTHOR/s/Dr. Fritz Mehner/YOUR NAME/'        $MainTemplateFile
+  sed --in-place  '/SetMacro.*AUTHORREF/s/fgm//'                           $MainTemplateFile
+  sed --in-place  '/SetMacro.*EMAIL/s/mehner.fritz@fh-swf.de//'            $MainTemplateFile
+  sed --in-place  '/SetMacro.*ORGANIZATION/s/FH Südwestfalen, Iserlohn//'  $MainTemplateFile
 else
-	echo -e "Datei ${templatefile} nicht gefunden!\n"
+  echo -e "Datei ${MainTemplateFile} nicht gefunden!\n"
 fi
 
 #-------------------------------------------------------------------------------
 #   Archiv erstellen
 #-------------------------------------------------------------------------------
-eval zip -r $archive_name . -x $exclude_list
+cd ..
+
+rm --force $archive_name'.zip'
+
+filelist="
+ ./autoload/mmtemplates/core.vim
+ ./autoload/perlsupportprofiling.vim
+ ./autoload/perlsupportregex.vim
+ ./doc/perlsupport.txt
+ ./ftplugin/perl.vim
+ ./ftplugin/pod.vim
+ ./ftplugin/qf.vim
+ ./perl-support/codesnippets/*
+ ./perl-support/doc/*
+ ./perl-support/modules/*
+ ./perl-support/rc/*
+ ./perl-support/README.perlsupport
+ ./perl-support/scripts/*
+ ./perl-support/templates/*
+ ./perl-support/wordlists/*
+ ./plugin/perl-support.vim
+"
+
+zip  $archive_name $filelist
 
