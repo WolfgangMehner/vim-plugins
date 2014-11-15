b768e4cfc7d78662b9e9325bec0a87a6e47a4f8b
Wolfgang Mehner <wolfgang-mehner@web.de>
2014-11-15 18:46:07 +0100

    Project: Added the vim2html script
    - Added 'vim2html.pl'.
    - Added 'make-html.sh', which is a wrapper around it.
    - Added 'filter-vimtags.pl', for historical reasons.
      It is now obsolete due to changes in 'vim2html.pl'.

---
+++ project/filter-vimtags.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  filter-vimtags.pl
+#
+#        USAGE:  filter-vimtags.pl [-i input file] [-o output file]
+#
+#  DESCRIPTION:  more remove stars from vim-doc labels, such as:
+#                  *csupport-run*
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  ---
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Dr. Fritz Mehner (fgm), mehner@fh-swf.de
+#      COMPANY:  FH Südwestfalen, Iserlohn
+#      VERSION:  1.0
+#      CREATED:  20.03.2009
+#     REVISION:  15.11.2014
+#===============================================================================
+
+use strict;
+use warnings;
+use Getopt::Std;                                # Process single-character switches
+
+our($opt_i, $opt_o);                            # declare package variables
+
+getopts('i:o:');                                # 
+
+$opt_i	= '-' unless defined $opt_i;            # if not set, set output to STDIN
+$opt_o	= '-' unless defined $opt_o;            # if not set, set output to STDOUT
+
+open  STDIN, "< $opt_i"
+	or die  "$0 : failed to open   input file '$opt_i' : $!\n";
+
+my	$rgx_vimtag	= q/<b class="vimtag">\*/;
+my	$line;
+my	@lines	= <STDIN>;
+
+close STDIN 
+	or warn "$0 : failed to close  input file '$opt_i' : $!\n";
+
+open  STDOUT, "+> $opt_o"
+	or die  "$0 : failed to open  output file '$opt_o' : $!\n";
+
+foreach my $line ( @lines ) {
+	if (  $line =~ m/$rgx_vimtag/ ) {
+		$line =~ s/(class="vimtag">)\*/$1/g;
+		$line =~ s#</a>\*</b>#</a></b>#g;
+	}
+	print $line;
+}
+
+close STDOUT 
+	or warn "$0 : failed to close output file '$opt_o' : $!\n";
+
---
+++ project/make-html.sh
@@ -0,0 +1,47 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  make-html.sh
+# 
+#         USAGE:  ./make-html.sh 
+# 
+#   DESCRIPTION:  performs these steps:
+#                 - switch to the ./doc directory
+#                 - do the helptags there
+#                 - run "vim2html.pl"
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Dr.-Ing. Fritz Mehner (fgm), mehner@fh-swf.de
+#       COMPANY:  Fachhochschule Südwestfalen, Iserlohn
+#       VERSION:  1.0
+#       CREATED:  20.03.2009
+#      REVISION:  15.11.2014
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+PROJECT_BASE_DIR=$(git rev-parse --show-toplevel)
+
+# go to doc/
+cd $PROJECT_BASE_DIR/doc
+
+# redo the helptags
+echo -e "producing the helptags ..."
+vim -c  ':helptags . | q'
+
+# run vim2html on all files
+if [ -r tags ]; then
+	echo -e "running vim2html ..."
+	$PROJECT_BASE_DIR/project/vim2html.pl tags *.txt
+else
+	echo -e "tag file not found, aborting"
+	exit 1
+fi
+
+# move all the produced files to doc-html/
+echo -e "moving results to doc-html/ ..."
+mkdir -p $PROJECT_BASE_DIR/doc-html
+mv *.css *.html $PROJECT_BASE_DIR/doc-html
